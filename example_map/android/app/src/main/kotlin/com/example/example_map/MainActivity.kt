3e8f0d8cbeacfbc48ee33b4c2d09b181eeed3990
MinhPC2k3 <pcminhbg@gmail.com>
2023-11-22 16:13:06 +0700

    first commit

--- example_map/android/app/src/main/kotlin/com/example/example_map/MainActivity.kt
+++
@@ -1,8 +0,0 @@
-package com.example.example_map
-
-import io.flutter.embedding.android.FlutterActivity
-
-
-class MainActivity: FlutterActivity() {
-}
-///home/pham/StudioProjects/flutter_test/example_map/android/app/src/main/kotlin/com/example/example_map/MainActivity.kt
